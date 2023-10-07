51509ae86245ac87d6d4c08228fbfe586e40f1a2
Sahil Rai <Raisahil580@gmail.com>
2023-10-07 01:13:20 -0700

    Project Section Updated

--- src/NavBars/ProjectNavBar/projectNavBar.js
+++ src/NavBars/ProjectNavBar/projectNavBar.js
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import Headings from '../../Components/SubHeaders/SubHeader'
-import './projectNavBar.css'
-import { Link } from 'react-router-dom'
+// ProjectNavBar.jsx
+
+import { useState } from 'react';
+import Headings from '../../Components/SubHeaders/SubHeader';
+import './projectNavBar.css';
+import { Link, useLocation } from 'react-router-dom';
 
 const ProjectNavBar = () => {
-    const [clickedLink, setClickedLink] = useState(null);
+    const location = useLocation();
+    const [clickedLink, setClickedLink] = useState(location.pathname);
 
-    const handleLickClicked = (index) => {
-        setClickedLink(index)
-    }
+    const handleLinkClicked = (path) => {
+        setClickedLink(path);
+    };
 
-    const linkStyles = (index) => {
+    const linkStyles = (path) => {
         return {
-            color: index ===clickedLink ? 'white' : '#AC9C9C'
-        }
-    }
+            color: path === clickedLink ? 'white' : '#AC9C9C',
+        };
+    };
 
     return (
         <>
-        <Headings context='Check out my' main_word='Projects' />
+            <Headings context='Check out my' main_word='Projects' />
             <div className='project-navbar'>
                 <div className='navbar-routes-container'>
-                    <div><Link to='/' className='link-style' style={ linkStyles(0) } onClick={() => handleLickClicked(0)}>Websites</Link></div>
-                    <div><Link to='/research' className='link-style' style={ linkStyles(1) } onClick={() => handleLickClicked(1)}>Research</Link></div>
-                    <div><Link to='/mobile-desktop-apps' className='link-style' style={ linkStyles(2) } onClick={() => handleLickClicked(2)}>Mobile and Desktop</Link></div>
-                    <div><Link to='/games' className='link-style' style={ linkStyles(3) } onClick={() => handleLickClicked(3)}>Games</Link></div>
-                    <div><Link to='/rest' className='link-style' style={ linkStyles(4) } onClick={() => handleLickClicked(4)}>Others/Ongoing</Link></div>
+                    <div>
+                        <Link
+                            to='/'
+                            className={`link-style ${location.pathname === '/' ? 'active-link' : ''}`}
+                            style={linkStyles('/')}
+                            onClick={() => handleLinkClicked('/')}
+                        >
+                            Websites
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            to='/research'
+                            className={`link-style ${location.pathname === '/research' ? 'active-link' : ''}`}
+                            style={linkStyles('/research')}
+                            onClick={() => handleLinkClicked('/research')}
+                        >
+                            Research
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            to='/mobile-desktop-apps'
+                            className={`link-style ${location.pathname === '/mobile-desktop-apps' ? 'active-link' : ''}`}
+                            style={linkStyles('/mobile-desktop-apps')}
+                            onClick={() => handleLinkClicked('/mobile-desktop-apps')}
+                        >
+                            Mobile and Desktop
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            to='/games'
+                            className={`link-style ${location.pathname === '/games' ? 'active-link' : ''}`}
+                            style={linkStyles('/games')}
+                            onClick={() => handleLinkClicked('/games')}
+                        >
+                            Games
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            to='/rest'
+                            className={`link-style ${location.pathname === '/rest' ? 'active-link' : ''}`}
+                            style={linkStyles('/rest')}
+                            onClick={() => handleLinkClicked('/rest')}
+                        >
+                            Others/Ongoing
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default ProjectNavBar
+export default ProjectNavBar;
